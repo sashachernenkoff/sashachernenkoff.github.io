a74a064bad8be0a71de4cb6582a3a97bf9b9f776
sashachernenkoff <sjchernenkoff@gmail.com>
2023-07-03 10:18:37 -0600

    added content to multiple pages
    added content to research.html, updated css for project pages (including js lightbox functions), added img assets for new content

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -34,6 +34,42 @@ function setMain2(thumbnailImage) {
     thumbnailImage.style.border = "0.5px solid #333";
 }
 
+function setMain3(thumbnailImage) {
+
+    // change the main image to the thumbnail that was clicked
+    var mainImage = document.getElementById("imageBox-3");
+    mainImage.src = thumbnailImage.src;
+
+    var childImages = document.getElementsByClassName("lb-thumbnail-3");
+    var i;
+
+    // clear all other borders
+    for ( i = 0; i < childImages.length; i++ ) {
+        childImages[i].style.border = "";
+    }
+
+    // add border to the thumbnail that was clicked
+    thumbnailImage.style.border = "0.5px solid #333";
+}
+
+function setMain5(thumbnailImage) {
+
+    // change the main image to the thumbnail that was clicked
+    var mainImage = document.getElementById("imageBox-5");
+    mainImage.src = thumbnailImage.src;
+
+    var childImages = document.getElementsByClassName("lb-thumbnail-5");
+    var i;
+
+    // clear all other borders
+    for ( i = 0; i < childImages.length; i++ ) {
+        childImages[i].style.border = "";
+    }
+
+    // add border to the thumbnail that was clicked
+    thumbnailImage.style.border = "0.5px solid #333";
+}
+
 function openModal(img) {
 
     // change the modal image to the thumbnail that was clicked
